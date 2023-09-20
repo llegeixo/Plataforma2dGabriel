64a20a93865243a1dae825de0396f5f856fb7cdd
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-09-20 10:26:22 +0200

    groundsensor commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float playerSpeed = 5;
-    private float playerInput;
-    private float playerInput2;
+    [SerializeField]private float jumpForce = 5;
+    private float playerInputHorizontal;
+    //private float playerInputVertical;
+
+    private Rigidbody2D rBody2D; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
        PlayerMovement();
+
+       if(Input.GetButtonDown("Jump"))
+       {
+            Jump();
+       }
+    }
+
+    void FixedUpdate()
+    {
+        //rBody2D.AddForce(new Vector2(playerInputHorizontal * playerSpeed, 0), ForceMode2D.Impulse);
+
+        rBody2D.velocity = new Vector2(playerInputHorizontal * playerSpeed, rBody2D.velocity.y);
     }
 
     void PlayerMovement()
     {
-        playerInput = Input.GetAxis("Horizontal");
-        playerInput2 = Input.GetAxis("Vertical");
+        playerInputHorizontal= Input.GetAxis("Horizontal");
+        /*playerInput2 = Input.GetAxis("Vertical");
         
-        transform.Translate(new Vector2(playerInput, playerInput2) * playerSpeed * Time.deltaTime);
+        transform.Translate(new Vector2(playerInput, playerInput2) * playerSpeed * Time.deltaTime);*/
+    }
+
+    void Jump()
+    {
+         rBody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
