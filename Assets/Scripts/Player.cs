431d267f3acdd8e94fa8cf273c6bc25361615e1f
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-09-27 09:45:02 +0200

    idle anim commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Stats")]
+    [Tooltip("Controla la velocidad de movimiento del personaje.")]
     [SerializeField]private float playerSpeed = 5;
+    [Tooltip("Controla la fuerza de salto del personaje.")]
     [SerializeField]private float jumpForce = 5;
     private float playerInputHorizontal;
     //private float playerInputVertical;
 
     private Rigidbody2D rBody2D; 
     private GroundSensor sensor;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody2D = GetComponent<Rigidbody2D>();
         sensor = GetComponentInChildren<GroundSensor>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -40,6 +45,16 @@ public class Player : MonoBehaviour
     void PlayerMovement()
     {
         playerInputHorizontal= Input.GetAxis("Horizontal");
+
+        if(playerInputHorizontal != 0)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
+        if(playerInputHorizontal == 0)
+        {
+            animator.SetBool("IsRunning", false);
+        }
         /*playerInput2 = Input.GetAxis("Vertical");
         
         transform.Translate(new Vector2(playerInput, playerInput2) * playerSpeed * Time.deltaTime);*/
