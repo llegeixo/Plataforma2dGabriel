af72be061ad9b4385a5f3fcc3ff910b6a37bccdb
llegeixo <gabriel.tallon.lopez@gmail.com>
2023-11-18 15:08:52 +0100

    music commit

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Star"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    
+}
